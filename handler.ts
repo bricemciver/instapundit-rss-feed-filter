610cf495604cae962bd352b159dd04bd87fae394
Brice McIver <bmciver@keyholesoftware.com>
2022-08-15 11:53:53 -0500

    Update to Serverless framework v3 and remove axios/use native http

--- handler.ts
+++ handler.ts
@@ -1,24 +1,39 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import axios from 'axios';
-import { parseStringPromise, Builder } from 'xml2js';
-import 'source-map-support/register';
+import { Handler } from "aws-lambda";
+import * as http from "node:http";
 
-export const feed: APIGatewayProxyHandler = async (_event, _context) => {
-  const response = await axios.get(
-    "http://feeds.feedburner.com/pjmedia/instapundit"
-  );
-  // filter out unwanted items
-  const xml = await parseStringPromise(response.data);
-  const items = xml.rss.channel[0].item.filter(
-    (item: any) => item["dc:creator"][0] === "Glenn Reynolds"
-  );
-  xml.rss.channel[0].item = items;
+import { parseStringPromise, Builder } from "xml2js";
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/rss+xml",
-    },
-    body: new Builder().buildObject(xml),
-  };
-}
+export const feed: Handler = () => {
+  return new Promise((resolve, reject) => {
+    http
+      .get("http://feeds.feedburner.com/pjmedia/instapundit", (resp) => {
+        let data = "";
+
+        // A chunk of data has been received.
+        resp.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on("end", async () => {
+          // filter out unwanted items
+          const xml = await parseStringPromise(data);
+          const items = xml.rss.channel[0].item.filter(
+            (item: any) => item["dc:creator"][0] === "Glenn Reynolds"
+          );
+          xml.rss.channel[0].item = items;
+
+          resolve({
+            statusCode: 200,
+            headers: {
+              "Content-Type": "application/rss+xml",
+            },
+            body: new Builder().buildObject(xml),
+          });
+        });
+      })
+      .on("error", (err) => {
+        reject(err);
+      });
+  });
+};
