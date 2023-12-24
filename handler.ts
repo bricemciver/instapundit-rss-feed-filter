b287a59d067bb3c4543fb99aa8b42359719d12d5
Brice McIver <github@bricemciver.com>
2023-12-23 18:09:05 -0600

    chore: add tsconfig and run eslint/prettier against code

--- handler.ts
+++ handler.ts
@@ -1,80 +1,82 @@
-import { Handler } from "aws-lambda";
-import * as http from "node:http";
+import { type Handler } from 'aws-lambda'
+import * as http from 'node:http'
 
-import { parseStringPromise, Builder } from "xml2js";
+import { parseStringPromise, Builder } from 'xml2js'
 
-const FEED_URL = "http://feeds.feedburner.com/pjmedia/instapundit";
+const FEED_URL = 'http://feeds.feedburner.com/pjmedia/instapundit'
 
-const getFeed = async () => {
-  return new Promise<string>((resolve, reject) => {
+const getFeed = async (): Promise<string> => {
+  return await new Promise<string>((resolve, reject) => {
     const req = http.get(FEED_URL, (res) => {
-      if (!res.statusCode || res.statusCode < 200 || res.statusCode >= 300) {
-        return reject(new Error("Status Code error"));
+      if (res.statusCode !== 200) {
+        reject(new Error('Status Code error'))
+        return
       }
 
-      const body: Uint8Array[] = [];
+      let body = ''
 
-      res.on("data", (chunk) => body.push(chunk));
+      res.on('data', (chunk) => {
+        body += chunk
+      })
 
-      res.on("end", () => {
-        const resString = Buffer.concat(body).toString();
-        resolve(resString);
-      });
-    });
+      res.on('end', () => {
+        resolve(body)
+      })
+    })
 
-    req.on("error", (err) => {
-      reject(err);
-    });
-  });
-};
+    req.on('error', (err) => {
+      reject(err)
+    })
+  })
+}
 
-const parseXml = async (xmlString: string) => {
-  return new Promise((resolve, reject) => {
+const parseXml = async (xmlString: string): Promise<any> => {
+  return await new Promise((resolve, reject) => {
     parseStringPromise(xmlString)
       .then((result) => {
-        resolve(result);
+        resolve(result)
       })
       .catch((err) => {
-        reject(err);
-      });
-  });
-};
+        reject(err)
+      })
+  })
+}
 
 // filter items by creator
-const filterXml = async (data) => {
+const filterXml = async (data): any => {
   const items = data.rss.channel[0].item.filter(
-    (item: any) => item["dc:creator"][0] === "Glenn Reynolds",
-  );
-  data.rss.channel[0].item = items;
-  return data;
-};
+    (item: any) => item['dc:creator'][0] === 'Glenn Reynolds'
+  )
+  data.rss.channel[0].item = items
+  return data
+}
 
-const buildOutput = async (parsedXml) => {
-  return new Promise<string>((resolve) => {
-    const builder = new Builder();
+const buildOutput = async (parsedXml): Promise<string> => {
+  return await new Promise<string>((resolve) => {
+    const builder = new Builder()
 
-    const xmlString = builder.buildObject(parsedXml);
+    const xmlString = builder.buildObject(parsedXml)
 
-    resolve(xmlString);
-  });
-};
+    resolve(xmlString)
+  })
+}
 
 export const feed: Handler = async () => {
   try {
-    const response = await getFeed();
-    const feedXml = await parseXml(response);
-    const filteredXml = await filterXml(feedXml);
-    const output = await buildOutput(filteredXml);
+    const response = await getFeed()
+    const feedXml = await parseXml(response)
+    const filteredXml = await filterXml(feedXml)
+    const output = await buildOutput(filteredXml)
 
     return {
       statusCode: 200,
-      body: output,
-    };
+      body: output
+    }
   } catch (err) {
     // handle errors
     return {
       statusCode: 500,
-      body: err,
-    };
+      body: err
+    }
   }
-};
+}
