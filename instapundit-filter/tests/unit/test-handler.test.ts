a40316c3ad393693d2c550817adcd5bc802fee69
Brice McIver <github@bricemciver.com>
2025-11-26 10:53:28 -0600

    Update dependencies and SAM template. Add Github Actions (#2)

--- instapundit-filter/tests/unit/test-handler.test.ts
+++ instapundit-filter/tests/unit/test-handler.test.ts
@@ -1,12 +1,12 @@
-import { APIGatewayProxyResult } from 'aws-lambda';
-import { lambdaHandler } from '../../app';
-import { expect, describe, it } from '@jest/globals';
+import { describe, expect, it } from "@jest/globals";
+import type { APIGatewayProxyResult } from "aws-lambda";
+import { lambdaHandler } from "../../app";
 
-describe('Unit test for app handler', function () {
-    it('verifies successful response', async () => {
-        const result: APIGatewayProxyResult = await lambdaHandler();
+describe("Unit test for app handler", () => {
+	it("verifies successful response", async () => {
+		const result: APIGatewayProxyResult = await lambdaHandler();
 
-        expect(result.statusCode).toEqual(200);
-        expect(result.body.length).toBeGreaterThan(0);
-    });
+		expect(result.statusCode).toEqual(200);
+		expect(result.body.length).toBeGreaterThan(0);
+	});
 });
